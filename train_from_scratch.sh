8c71526de5dab5b4fa4ae03fd07502c20777d6ba
pixcel8 <pixce@outlook.com>
2026-07-22 08:21:05 +0100

    Update train_from_scratch.sh

--- train_from_scratch.sh
+++ train_from_scratch.sh
@@ -1,10 +1,8 @@
 export HF_HOME=/mnt/l/Datasets/HuggingFace
 export HF_DATASETS_CACHE=/mnt/l/Datasets/HuggingFace/cache
 export HF_HUB_DISABLE_SYMLINKS_WARNING="1"
-#mkdir -p /mnt/l/Datasets/HuggingFace/cache
-#echo "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" > /mnt/l/Datasets/HuggingFace/cache/stored_tokens
-export HF_TOKEN="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"
+export HF_TOKEN=[HF TOKEN NEEDED]
 
-huggingface-cli login --token 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
+huggingface-cli login --token [HF TOKEN NEEDED]
 python train_data_prep.py
 python train_from_scratch.py
